ace68d851e172f7e7d16194b8e3d1a8989debd47
Alex Balak <alex.balak@outlook.com>
2025-06-21 23:58:01 +0200

    Start Over

--- src/main/java/alex/blog/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package alex.blog.repository;
-
-import alex.blog.model.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository <Author, Integer> {
-}
--- src/main/java/alex/blog/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package alex.blog.repository;
-
-import alex.blog.model.Post;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepository extends CrudRepository <Post, Integer> {
-}
